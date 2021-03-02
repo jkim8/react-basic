128738977608be82fa6f5f02b244c538b7770497
naujkim <jkim@pbsus.com>
2021-03-02 16:56:03 -0500

    auth and logout

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const config = require('./config/key')
+const { auth } = require('./middleware/auth')
 const { User } = require('./Models/User')
 
 
@@ -22,11 +23,11 @@ mongoose.connect(config.mongoURI, {
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('워이워이')
+  res.send('야래야래')
 })
 
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
    //회원가입할때 필요한 정보들을 클라이언트에서 가져오면 
    //그것들을 데이터 베이스에 넣어준다. 
@@ -42,7 +43,7 @@ app.post('/register', (req, res) => {
 })
 
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   //요청된 이메일을 데이터베이스에서 있는지 찾는다
   User.findOne({ email: req.body.email }, (err, user) => {
     if(!user) {
@@ -64,13 +65,42 @@ app.post('/login', (req, res) => {
         if(err) return res.status(400).send(err)
 
         // token을 저장한다 어디에 ? 쿠키 , 로컬스토리지, 세션
-        res.cookie("X-auth", user.token)
+        res.cookie("x_auth", user.token)
         .status(200)
         .json({ success: true, userId: user._id })   
         
       })
     })
   })
+})
+
+//auth 는 미들웨어 
+app.get('/api/users/auth', auth, (req, res) => {
+
+  //여기까지 미들웨어를 통과해 왔다는 얘기는 authentication이 true 라는 말
+  res.status(200).json({ 
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    mane: req.user.name, 
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+   })
+})
+
+
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+  User.findOneAndUpdate({ _id: req.user._id}, 
+    { token: ""},
+    (err, user) => {
+      if(err) return res.json({ success: false, err })
+      return res.status(200).send({ success: true })
+    })
+
 })
 
 
